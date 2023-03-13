d8858571be909eb260b6149345891120ea7b80bd
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2023-03-13 11:01:52 -0300

    [FEAT] Project setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css';
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <h1>SSW Tracking</h1>
+      <p>Test</p>
+    </div>
+  );
+}
